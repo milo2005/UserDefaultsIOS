94a220e6f0369260d1b16617f28bc5f6e9824810
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-11-01 18:23:49 -0500

    Initial Commit

---
+++ UserDefault/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefault
+//
+//  Created by Aplimovil on 11/1/16.
+//  Copyright © 2016 Aplimovil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
